b07aeb1420fc3ec46cf2651cfaeaba20a6bb8d6f
Abdul wali khan Afridi <161517053+Abdulwali110@users.noreply.github.com>
2024-04-04 15:43:27 +0500

    Add files via upload

---
+++ seven.ts
@@ -0,0 +1,8 @@
+let addition:number= 6+2
+console.log(addition);
+let subtract:number= 10-2
+console.log(subtract);
+let multiplication:number=2*4
+console.log(multiplication);
+let division:number= 16/2
+console.log(division);
