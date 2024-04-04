d450fc9bdd67f1ba69c1e93c6ff371b8483d0543
Abdul wali khan Afridi <161517053+Abdulwali110@users.noreply.github.com>
2024-04-04 02:40:12 -0800

    Delete siven.js

--- siven.js
+++
@@ -1,8 +0,0 @@
-var addition = 6 + 2;
-console.log(addition);
-var subtract = 10 - 2;
-console.log(subtract);
-var multiplication = 2 * 4;
-console.log(multiplication);
-var division = 16 / 2;
-console.log(division);
